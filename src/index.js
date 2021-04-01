9f171682049b76d729d50bd693a7cba51920bb87
rowjay007 <rowlives@gmail.com>
2021-04-01 12:41:12 +0100

    installing redux react-redux to project

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App  unnecessary />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
