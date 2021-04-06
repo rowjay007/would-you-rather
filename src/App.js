af11a486fe1b8a755860ed8e8054dc9c4f171c93
rowjay007 <rowlives@gmail.com>
2021-04-06 10:07:30 +0100

    wrapping app component with connect()

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import "./App.css";
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
+import { handleInitialData } from '../actions/shared'; 
+import { connect } from 'react-redux'; 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello All</h1>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() { 
+    this.props.handleInitialData(); 
+  } 
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <ContentGrid>
+            <p>New Start...</p>
+          </ContentGrid>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+const ContentGrid = ({ children }) => (
+  <Grid padded="vertically" columns={1} centered>
+    <Grid.Row>
+      <Grid.Column style={{ maxWidth: 550 }}>{children}</Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
+export default connect( 
+  null, 
+  { handleInitialData } 
+)(App);
