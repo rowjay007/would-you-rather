6221289957fe80859d593bd0cf00362a5d68b369
rowjay007 <rowlives@gmail.com>
2021-04-14 11:42:04 +0100

    creating MarkUserAnswer file

---
+++ src/components/Questions/QuestionStats/MarkUserAnswer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MarkUserAnswer = () => (
+  <p>
+    <span style={{ color: "tomato", fontSize: "1.5rem", height: "20px" }}>
+      &#10003;
+    </span>
+    Your Answer
+  </p>
+);
+
+export default MarkUserAnswer;
