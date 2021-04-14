78d345c0852aac70eb3d8f0272dbfa187706250e
rowjay007 <rowlives@gmail.com>
2021-04-14 09:03:59 +0100

    adding UserSummary in Leaderboard components

---
+++ src/components/Leaderboard/UserSummary.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./Leaderboard.module.css";
+
+// takes in a user object and returns UserSummary component for LeaederBoard
+const UserSummary = (props) => {
+  const { name } = props;
+  return (
+    <div key={name.id} className={classes.container}>
+      <div className={classes.imgContainer}>
+        <img
+          className={classes.img}
+          src={name.avatar}
+          alt={`${name.name}'s avatar`}
+        />
+      </div>
+      <div className={classes.info}>
+        <h3 className={classes.name}>{name.name}</h3>
+        <p className={classes.scoreItem}>
+          <span>Answered questions</span>
+          <span>{name.answered}</span>
+        </p>
+        <p className={classes.scoreItem}>
+          <span>Created questions</span>
+          <span>{name.asked}</span>
+        </p>
+      </div>
+      <div className={classes.score}>
+        <p>score</p>
+        <div className={classes.tomato}>{name.total}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UserSummary;
