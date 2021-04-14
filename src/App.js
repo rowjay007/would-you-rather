5f0e67b3d8ccedb1649b6b694836b8a9aa0c59ce
rowjay007 <rowlives@gmail.com>
2021-04-14 08:54:39 +0100

    fixing App components file

--- src/App.js
+++ src/App.js
@@ -1,67 +1,54 @@
-import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Grid } from "semantic-ui-react";
-import { handleInitialData } from "./actions/shared";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import Login from "./components/Login";
-import Nav from './components/Nav';
-import Home from "./components/Home";
-import UserCard from "./components/UserCard";
-import NewPoll from "./components/NewPoll";
-import Leaderboard from "./components/Leaderboard";
-import NoMatch from "./components/NoMatch";
+import { Switch, Route, Redirect } from "react-router-dom";
+import LoadingBar, { showLoading, hideLoading } from "react-redux-loading-bar";
+import { getUsers, getQuestions } from "./Actions";
+import Nav from "./Components/Nav/Nav";
+import Login from "./Components/Login/Login";
+import NewQuestion from "./Components/NewQuestion/NewQuestion";
+import Leaderboard from "./Components/Leaderboard/Leaderboard";
+import Questions from "./Components/Questions/Questions";
+import QuestionItem from "./Components/Questions/QuestionItem/QuestionItem";
+import NoMatch from "./Components/NoMatch/NoMatch";
+import PrivateRoute from "./Components/HOC/PrivateRoute";
+
+import "./App.css";
 
 class App extends Component {
   componentDidMount() {
-    this.props.handleInitialData();
+    const { dispatch } = this.props;
+    dispatch(showLoading());
+    dispatch(getUsers());
+    dispatch(getQuestions()).then(() => dispatch(hideLoading()));
   }
+
   render() {
-    const { authUser } = this.props;
     return (
-      <Router>
-        <div className="App">
-          {authUser === null ? (
-            <Route
-              render={() => (
-                <ContentGrid>
-                  <Login />
-                </ContentGrid>
-              )}
-            />
-          ) : (
-              <Fragment>
-                
-              <Nav />
-              <ContentGrid>
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/questions/bad_id" component={NoMatch} />
-                  <Route path="/questions/:question_id" component={UserCard} />
-                  <Route path="/add" component={NewPoll} />
-                  <Route path="/leaderboard" component={Leaderboard} />
-                  <Route component={NoMatch} />
-                </Switch>
-              </ContentGrid>
-            </Fragment>
-          )}
-        </div>
-      </Router>
+      <div className="App">
+        <LoadingBar />
+        <React.Fragment>
+          <Nav authedUser={this.props.authedUser} />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <PrivateRoute path="/home" component={Questions} />
+            <Redirect exact from="/" to="/home" />
+            <PrivateRoute path="/add" component={NewQuestion} />
+            <PrivateRoute path="/leaderboard" component={Leaderboard} />
+            <PrivateRoute path="/questions/:id" component={QuestionItem} />
+            <PrivateRoute path="*" component={NoMatch} />
+            <Route component={NoMatch} />
+          </Switch>
+        </React.Fragment>
+      </div>
     );
   }
 }
 
-const ContentGrid = ({ children }) => (
-  <Grid padded="vertically" columns={1} centered>
-    <Grid.Row>
-      <Grid.Column style={{ maxWidth: 550 }}>{children}</Grid.Column>
-    </Grid.Row>
-  </Grid>
-);
+const mapStateToProps = ({ users, questions, authedUser, loadingBar }) => ({
+  users,
+  questions,
+  authedUser,
+  loadingBar,
+});
 
-function mapStateToProps({ authUser }) {
-  return {
-    authUser,
-  };
-}
-
-export default connect(mapStateToProps, { handleInitialData })(App);
+export default connect(mapStateToProps)(App);
