b10dbff6d17ada92ac44fe90dbeeb81b438b98f5
rowjay007 <rowlives@gmail.com>
2021-04-14 09:01:43 +0100

    fixing Leaderboard components

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,83 +1,37 @@
-import React, { Component, Fragment } from "react";
-import PropType from "prop-types";
+import React from "react";
 import { connect } from "react-redux";
-import {
-  Segment,
-  Grid,
-  Header,
-  Image,
-  Label,
-  Divider,
-} from "semantic-ui-react";
+import UserSummary from "./UserSummary";
+import classes from "./Leaderboard.module.css";
 
-const trophyColor = ["yellow", "grey", "orange"];
+const Leaderboard = (props) => {
+  const { users } = props;
+  const names = users ? Object.keys(users) : null;
+  const formated =
+    names !== null
+      ? names
+          .map((name) => ({
+            id: users[name].id,
+            name: users[name].name,
+            asked: users[name].questions.length,
+            answered: Object.keys(users[name].answers).length,
+            total:
+              Object.keys(users[name].answers).length +
+              users[name].questions.length,
+            avatar: users[name].avatarURL,
+          }))
+          .sort((a, b) => b.total - a.total)
+      : [];
+  return (
+    <div className={classes.mainContainer}>
+      {!formated.length
+        ? null
+        : formated.map((name) => <UserSummary key={name.id} name={name} />)}
+    </div>
+  );
+};
 
-export class Leaderboard extends Component {
-  static propType = {
-    leaderboardData: PropType.array.isRequired,
-  };
-  render() {
-    const { leaderboardData } = this.props;
-
-    return (
-      <Fragment>
-        {leaderboardData.map((user, idx) => (
-          <Segment.Group key={user.id}>
-            <Label corner="left" icon="trophy" color={trophyColor[idx]} />
-            <Grid divided padded>
-              <Grid.Row>
-                <Grid.Column width={4} verticalAlign="middle">
-                  <Image src={user.avatarURL} />
-                </Grid.Column>
-                <Grid.Column width={8}>
-                  <Header as="h3" textAlign="left">
-                    {user.name}
-                  </Header>
-                  <Grid>
-                    <Grid.Column width={12}>Answered questions</Grid.Column>
-                    <Grid.Column width={4}>{user.answerCount}</Grid.Column>
-                  </Grid>
-                  <Divider />
-                  <Grid>
-                    <Grid.Column width={12}>Created questions</Grid.Column>
-                    <Grid.Column width={4}>{user.questionCount}</Grid.Column>
-                  </Grid>
-                </Grid.Column>
-                <Grid.Column width={4} textAlign="center">
-                  <Segment.Group>
-                    <Header as="h5" block attached="top" content="Score" />
-                    <Segment>
-                      <Label circular color="green" size="big">
-                        {user.questionCount + user.answerCount}
-                      </Label>
-                    </Segment>
-                  </Segment.Group>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment.Group>
-        ))}
-      </Fragment>
-    );
-  }
-}
-
-function mapStateToProps({ users }) {
-  const leaderboardData = Object.values(users)
-    .map((user) => ({
-      id: user.id,
-      name: user.name,
-      avatarURL: user.avatarURL,
-      answerCount: Object.values(user.answers).length,
-      questionCount: user.questions.length,
-      total: Object.values(user.answers).length + user.questions.length,
-    }))
-    .sort((a, b) => a.total - b.total)
-    .reverse()
-    .slice(0, 3);
-  return {
-    leaderboardData,
-  };
-}
+const mapStateToProps = ({ users }) => ({
+  users,
+});
 
 export default connect(mapStateToProps)(Leaderboard);
