d9c8a8d9478ff57d26de35bff3417d112353c5f5
rowjay007 <rowlives@gmail.com>
2021-04-14 08:47:51 +0100

    fixing questions file in action folder

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,40 +1,18 @@
-import { saveQuestion } from "../utils/api";
-import { addQuestionToUser } from "../actions/users";
-
 export const RECEIVE_QUESTIONS = "RECEIVE_QUESTIONS";
-export const ADD_ANSWER_TO_QUESTION = "ADD_ANSWER_TO_QUESTION";
-export const ADD_QUESTION = "ADD_QUESTION";
+export const SAVE_QUESTION = "SAVE_QUESTION";
+export const ANSWER_QUESTION = "ANSWER_QUESTION";
 
-export function receiveQuestions(questions) {
-  return {
-    type: RECEIVE_QUESTIONS,
-    questions,
-  };
-}
+export const receiveQuestions = (questions) => ({
+  type: RECEIVE_QUESTIONS,
+  questions,
+});
 
-export function addAnswerToQuestion(authUser, qid, answer) {
-  return {
-    type: ADD_ANSWER_TO_QUESTION,
-    authUser,
-    qid,
-    answer,
-  };
-}
+export const saveQuestion = (question) => ({
+  type: SAVE_QUESTION,
+  question,
+});
 
-function addQuestion(question) {
-  return {
-    type: ADD_QUESTION,
-    question,
-  };
-}
-
-export function handleSaveQuestion(optionOneText, optionTwoText, author) {
-  return (dispatch) => {
-    return saveQuestion({ optionOneText, optionTwoText, author }).then(
-      (question) => {
-        dispatch(addQuestion(question));
-        dispatch(addQuestionToUser(question));
-      }
-    );
-  };
-}
+export const answerQuestion = (answerInfo) => ({
+  type: ANSWER_QUESTION,
+  answerInfo,
+});
