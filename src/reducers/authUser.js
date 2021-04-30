75f72fba4461e9b72d9fb59a256b90adeffe823d
rowjay007 <rowlives@gmail.com>
2021-04-30 14:43:14 +0100

    adding logOut user to reducers

--- src/reducers/authUser.js
+++ src/reducers/authUser.js
@@ -1,9 +1,11 @@
-import { SET_AUTH_USER } from "../actions/authUser";
+import { LOGOUT_USER, SET_AUTH_USER } from "../actions/authUser";
 
 export default function authUser(state = null, action) {
   switch (action.type) {
     case SET_AUTH_USER:
       return action.id;
+    case LOGOUT_USER:
+      return null;
     default:
       return state;
   }
